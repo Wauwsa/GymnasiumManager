c4b104c3530bc733a0d4e36bea481dbeb01734dc
Wauwsa <rotblauhase@gmail.com>
2022-10-03 21:14:54 +0200

    Fixed success variable

--- loginForm/views.py
+++ loginForm/views.py
@@ -3,19 +3,16 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
-global success
-success = False  # success is variable if user has logged in successfully
 
 
 def login_user(request):
-    global success
     if request.method == "POST":  # if user goes to webpage and fills out form, do something
         uname = request.POST['uname']
         password = request.POST['password']
         user = authenticate(request, username=uname, password=password)
         if user is not None:
             login(request, user)
-            success = True  # set the variable to True
+            request.session['success'] = True
             return redirect('mainApp:home')  # Redirect to a success page.
         else:
             messages.success(request, "The credentials are either wrong or that account does not exist.")
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from loginForm.views import success
 
 # Create your views here.
 
 
 def main_page(request):
-    global success
-    print(success)
-    if success:  # if variable is true, that means user just logged in
-        messages.success(request, "You've successfully logged in!")  # success message
-        success = False  # set success message to False
+    if request.session['success']:  # if variable is true, that means user just logged in
+        messages.success(request, "You've successfully been logged in!")  # success message
+        request.session['success'] = False
     return render(request, 'index.html', {})
