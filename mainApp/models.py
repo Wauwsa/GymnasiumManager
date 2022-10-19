7067a1b0438cddfa3280c30a28dcf890b591eae0
Wauwsa <rotblauhase@gmail.com>
2022-10-19 15:16:22 +0200

    Added grade Model

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class Grade(models.Model):
+    grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(6)])
+    date = models.DateField()
