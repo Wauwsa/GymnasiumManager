828d3530e6cec10ac8304a2c296a83077a1883a9
Wauwsa <rotblauhase@gmail.com>
2022-10-20 17:26:28 +0200

    Changed Test Model

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.contrib.auth.models import AbstractUser
+import datetime
 # Create your models here.
 
 
-class Grade(models.Model):
-    grade = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(6)])
-    date = models.DateField()
+class Test(models.Model):
+    grade = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(6)])
+    date = models.DateField(default=datetime.date.today)
+
+    def __str__(self):
+        return f'Note: {self.grade}'
