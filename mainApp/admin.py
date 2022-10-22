8ccbb3fab85c93400ea786da7074d8ce5df05035
Wauwsa <rotblauhase@gmail.com>
2022-10-22 15:22:04 +0200

    Fixed Student Model to inherit AbstractUser

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,7 +1,59 @@
 from django.contrib import admin
 from .models import Test, Subject, Student
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class CustomStudentAdmin(UserAdmin):
+    list_display = (
+        'username', 'email', 'first_name', 'last_name', 'is_staff',
+        'birth', 'street', 'street_number', 'city', 'province', 'code'
+        )
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions'
+                )
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+        ('Additional info', {
+            'fields': ('birth', 'street', 'street_number', 'city', 'province', 'code')
+        })
+    )
+
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'password1', 'password2')
+        }),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions'
+                )
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+        ('Additional info', {
+            'fields': ('birth', 'street', 'street_number', 'city', 'province', 'code')
+        })
+    )
+
+
+
+
 admin.site.register(Test)
 admin.site.register(Subject)
-admin.site.register(Student)
+admin.site.register(Student, CustomStudentAdmin)
--- mainApp/models.py
+++ mainApp/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 import datetime
 # Create your models here.
 
 
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Student(AbstractUser):
     birth = models.DateField(default=datetime.date.today)
-    street = models.CharField(max_length=25)
-    street_number = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(99)])
-    city = models.CharField(max_length=20)
-    province = models.CharField(max_length=20)
-    code = models.IntegerField(validators=[MinValueValidator(0, MaxValueValidator(9999))])
+    street = models.CharField(max_length=25, blank=True, null=True)
+    street_number = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(99)], blank=True, null=True)
+    city = models.CharField(max_length=20, blank=True, null=True)
+    province = models.CharField(max_length=20, blank=True, null=True)
+    code = models.IntegerField(validators=[MinValueValidator(0, MaxValueValidator(9999))], blank=True, null=True)
 
     def return_address(self):
-        address = self.street + ' ' + str(self.street_number) + ' ' + self.city + ' ' + str(self.code) + ' ' + self.province
-        return address
+        if self.birth and self.street and self.street_number and self.city and self.province and self.code:
+            return self.street + ' ' + str(self.street_number) + ' ' + self.city + ' ' + str(self.code) + ' ' + self.province
+        else:
+            return 'N/A'
 
     def __str__(self):
-        address = self.return_address()
-        return f'Adresse: {address}'
+        return f'{self.first_name + " " + self.last_name}'
 
 
 class Test(models.Model):
