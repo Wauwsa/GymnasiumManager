8e4b753f0f05cf5bc089ab89c4a0675668e0ad21
Wauwsa <rotblauhase@gmail.com>
2022-10-22 13:51:37 +0200

    Added Student Model

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 import datetime
 # Create your models here.
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth = models.DateField(default=datetime.date.today)
+    street = models.CharField(max_length=25)
+    street_number = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(99)])
+    city = models.CharField(max_length=20)
+    province = models.CharField(max_length=20)
+    code = models.IntegerField(validators=[MinValueValidator(0, MaxValueValidator(9999))])
+
+    def return_address(self):
+        address = self.street + ' ' + str(self.street_number) + ' ' + self.city + ' ' + str(self.code) + ' ' + self.province
+        return address
+
+    def __str__(self):
+        address = self.return_address()
+        return f'Adresse: {address}'
 
 
 class Test(models.Model):
@@ -11,3 +29,10 @@ class Test(models.Model):
 
     def __str__(self):
         return f'Note: {self.grade}'
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.name}'
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.contrib.auth import logout
+from .models import Subject, Student
 
 # Create your views here.
 # POST Logout muss nur in der View für Index getestet werden, da in HTML action="." --> Post wird zu root weitergeleitet
@@ -21,7 +21,11 @@ def main_page(request):
 
 def noten(request):
     if request.user.is_authenticated:  # if user is logged in
-        return render(request, 'noten.html')
+        object_list = Subject.objects.order_by('name')
+        subject_list = []
+        for instance in object_list:
+            subject_list.append(instance.name)
+        return render(request, 'noten.html', {'subject_list': subject_list})
     else:  # else redirect to login page
         return redirect('loginForm:login')
 
